2d0779346dd00e81a7df428c892f1e0328ca0d74
Danielon123456789 <h_gdaniel@hotmail.com>
2025-09-01 18:34:17 -0600

    flex: ahora si la buena eslint
    solucion ahora si

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"], 
+    languageOptions: { globals: globals.browser },
+  },
 ]);
