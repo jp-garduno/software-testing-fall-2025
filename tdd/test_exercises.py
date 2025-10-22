32fb8be616a9898775f48c495cc9d9561432a96c
Garduno Calderon, Juan P <juan.p.garduno.calderon@intel.com>
2025-10-22 11:36:03 -0600

    chore: add tests for negative numbers
    Test negative numbers for the FizzBuzz function.

--- tdd/test_exercises.py
+++ tdd/test_exercises.py
@@ -1,11 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Test Driven Development (TDD) tests.
+"""
 import unittest
 
 from tdd.exercises import fizzbuzz
 
+
 class TestFizzBuzz(unittest.TestCase):
     """
     Kata 1 - FizzBuzz
-    FizzBuzz is one of the most famous coding exercises for beginners. It is a simple exercise but an excellent one to start learning the TDD flow with.
+    FizzBuzz is one of the most famous coding exercises for beginners.
+    It is a simple exercise but an excellent one to start learning the TDD flow with.
 
     Requirements
     1. Write a “fizzBuzz” method that accepts a number as input and returns it as a String.
@@ -23,21 +30,41 @@ class TestFizzBuzz(unittest.TestCase):
     4. For numbers that are multiples of both three and five return “FizzBuzz”.
     """
 
-    def test_fizzbuzz_returns_number_as_string(self):
+    def test_fizzbuzz_should_return_number_as_string_when_number_is_not_multiple_of_3_or_5(
+        self,
+    ):
+        """
+        Tests that fizzbuzz returns the number as a string for non-multiples of 3 or 5.
+        """
         self.assertEqual(fizzbuzz(1), "1")
         self.assertEqual(fizzbuzz(2), "2")
+        self.assertEqual(fizzbuzz(-2), "-2")
 
-    def test_fizzbuzz_returns_fizz_for_multiples_of_three(self):
+    def test_fizzbuzz_should_return_fizz_when_number_is_multiple_of_three(self):
+        """
+        Tests that fizzbuzz returns "Fizz" for multiples of three.
+        """
         self.assertEqual(fizzbuzz(3), "Fizz")
         self.assertEqual(fizzbuzz(6), "Fizz")
         self.assertEqual(fizzbuzz(9), "Fizz")
+        self.assertEqual(fizzbuzz(-9), "Fizz")
 
-    def test_fizzbuzz_returns_buzz_for_multiples_of_five(self):
+    def test_fizzbuzz_should_return_buzz_when_number_is_multiple_of_five(self):
+        """
+        Tests that fizzbuzz returns "Buzz" for multiples of five.
+        """
         self.assertEqual(fizzbuzz(5), "Buzz")
         self.assertEqual(fizzbuzz(10), "Buzz")
         self.assertEqual(fizzbuzz(20), "Buzz")
+        self.assertEqual(fizzbuzz(-20), "Buzz")
 
-    def test_fizzbuzz_returns_fizzbuzz_for_multiples_of_three_and_five(self):
+    def test_fizzbuzz_should_return_fizzbuzz_when_number_is_multiple_of_three_and_five(
+        self,
+    ):
+        """
+        Tests that fizzbuzz returns "FizzBuzz" for multiples of both three and five.
+        """
         self.assertEqual(fizzbuzz(15), "FizzBuzz")
         self.assertEqual(fizzbuzz(30), "FizzBuzz")
         self.assertEqual(fizzbuzz(45), "FizzBuzz")
+        self.assertEqual(fizzbuzz(-45), "FizzBuzz")
