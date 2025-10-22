ce079815ab8fae5c5db8cf42be40e9a1d42d6565
Garduno Calderon, Juan P <juan.p.garduno.calderon@intel.com>
2025-10-22 12:12:59 -0600

    feat: add tdd exercise 2 v1
    Add code and tests for the requirement #1 for TDD exercise #2.

--- tdd/exercises.py
+++ tdd/exercises.py
@@ -38,3 +38,27 @@ def fizzbuzz(num):
         output = str(num)
 
     return output
+
+
+def add(numbers):
+    """
+    Kata 2 - String calculator
+    Create a simple calculator that takes a String and returns a integer.
+
+    Requirements
+    1. The method can take up to two numbers, separated by commas, and will return their sum as a
+    result. So the inputs can be: “”, “1”, “1,2”. For an empty string, it will return 0.
+
+    Notes:
+    Start with the simplest case (empty string) and extend it with the more advanced cases
+    (“1” and “1,2”) step by step keep the three rules in mind and always write just
+    sufficient enough code do not forget to refactor your code after each passing test.
+    """
+    if numbers == "":
+        return 0
+
+    num_list = numbers.split(",")
+    if len(num_list) == 1:
+        return int(num_list[0])
+
+    return int(num_list[0]) + int(num_list[1])
